c599b0fbc92550ef527331fe6489ae46403b578a
sidthefaker <sidthefaker@gmail.com>
2018-04-03 00:24:04 +0900

    add test code  * separate normal case and quit command  * organize codes

--- python/no1/hello_test.py
+++ python/no1/hello_test.py
@@ -1,9 +1,12 @@
 import unittest
 import hello
 
+
 class TestHello(unittest.TestCase):
     def test_hello(self):
         self.assertEqual(hello.hello("Brian"), "Hello, Brian, nice to meet you!", "invalid result in normal case")
+
+    def test_byebye(self):
         self.assertEqual(hello.hello("quit"), "Bye-bye", "\'quit\' command is not accepted")
 
 if __name__ == '__main__':
