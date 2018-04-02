bc93d99321ddf1cef3bfed11dfeda0543bd3b7ee
sidthefaker <sidthefaker@gmail.com>
2018-04-03 00:47:38 +0900

    initiate ex2(python)  * print input string its own  * use 1 print

---
+++ ex2/python/count_string_len.py
@@ -0,0 +1,7 @@
+def count_string_len(string):
+    pass
+
+
+if __name__ == '__main__':
+    string = input("What is the input string?")
+    print string + " has " + str(count_string_len(string)) + " characters."
