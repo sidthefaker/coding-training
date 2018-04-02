fc486c1b5f7965f2da4169be9ae18187e68b591c
sidthefaker <sidthefaker@gmail.com>
2018-04-03 01:03:40 +0900

    add test case  * add zero string test case

--- ex2/python/count_string_len.py
+++ ex2/python/count_string_len.py
@@ -2,6 +2,10 @@ def count_string_len(string):
     pass
 
 
+class ZeroStringError(ValueError):
+    '''input string must not be 0'''
+
+
 if __name__ == '__main__':
     string = input("What is the input string?")
     print string + " has " + str(count_string_len(string)) + " characters."
--- ex2/python/count_string_len_test.py
+++ ex2/python/count_string_len_test.py
@@ -1,7 +1,12 @@
 import unittest
 import count_string_len
 
+
 class TestCountStringLen(unittest.TestCase):
     def test_count(self):
         test_msg = "testtesttest"
         self.assertEqual(count_string_len.count_string_len(test_msg), len(test_msg), "invalid length")
+
+    def test_zero_string(self):
+        with self.assertRaises(count_string_len.ZeroStringError):
+            count_string_len.count_string_len("")
