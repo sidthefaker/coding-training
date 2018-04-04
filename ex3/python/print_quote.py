ca6f75a560fae9b08a86c67d42957c9181528fca
sidthefaker <sidthefaker@gmail.com>
2018-04-04 23:49:52 +0900

    initiate ex3 print quote

---
+++ ex3/python/print_quote.py
@@ -0,0 +1,8 @@
+def print_quote(speaker, words):
+    pass
+
+
+if __name__ == '__main__':
+    words = raw_input("What is the quote?")
+    speaker = raw_input("Who said is?")
+    print print_quote(speaker, words)
