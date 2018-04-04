ed728e19414472bdd93d9299831b75ff05c6b1bb
sidthefaker <sidthefaker@gmail.com>
2018-04-05 00:09:53 +0900

    implement ex3  * implement challenge store words in dictionary

--- ex3/python/print_quote.py
+++ ex3/python/print_quote.py
@@ -3,6 +3,19 @@ def print_quote(speaker, words):
 
 
 if __name__ == '__main__':
-    words = raw_input("What is the quote?")
-    speaker = raw_input("Who said is?")
-    print print_quote(speaker, words)
+    history = {}
+    while True:
+        words = raw_input("What is the quote?")
+        if words == "q": break;
+
+        speaker = raw_input("Who said is?")
+        if speaker == "q": break
+
+        if not history.has_key(speaker):
+            history[speaker] = []
+        history[speaker].append(words)
+
+        for s, ws in history.iteritems():
+            for w in ws:
+                print print_quote(s, w)
+
